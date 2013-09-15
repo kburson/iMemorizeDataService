c764833e32befb4e1ed675722240c2a654b5c807
Kendrick Burson <kburson@rallydev.com>
2013-09-15 10:35:02 -0700

    create grails-2.3.0 project from scratch to experiment using new REST domain objects

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,99 +1,45 @@
-//dataSource {
-//    pooled = true
-//    driverClassName = "org.h2.Driver"
-//    username = "sa"
-//    password = ""
-//}
-//hibernate {
-//    cache.use_second_level_cache = true
-//    cache.use_query_cache = false
-//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
-//}
-def mongoPort = 27017
-def mongoHost = System.getProperty("ORCA_MONGODB_HOST") ? System.getProperty("ORCA_MONGODB_HOST") : (System.getenv("ORCA_MONGODB_HOST") ? System.getenv("ORCA_MONGODB_HOST") : "localhost")
-def mongoDBName = "iMemorizeData"
+dataSource {
+    pooled = true
+    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+//    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+}
 
 // environment specific settings
 environments {
     development {
-        grails {
-            mongo {
-                port = mongoPort
-                host = mongoHost
-                databaseName = mongoDBName
-            }
-        }
         dataSource {
-            dbCreate = "create-drop"
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
     test {
-        grails {
-            mongo {
-                port = mongoPort
-                host = mongoHost
-                databaseName = mongoDBName
-            }
-        }
         dataSource {
-            dbCreate = "create-drop"
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
-//    ci {
-//        grails {
-//            mongo {
-//                host = "bld-testmdb-01"
-//                databaseName = "hello_service_test_ci"
-//            }
-//        }
-//        dataSource {
-//            dbCreate = "create-drop"
-//        }
-//    }
-
-//    production {
-//        grails {
-//            mongo {
-//                port = mongoPort
-//                host = mongoHost
-//                databaseName = mongoDBName
-//            }
-//        }
-//        dataSource {
-//            dbCreate = "update"
-//        }
-//    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            properties {
+               maxActive = -1
+               minEvictableIdleTimeMillis=1800000
+               timeBetweenEvictionRunsMillis=1800000
+               numTestsPerEvictionRun=3
+               testOnBorrow=true
+               testWhileIdle=true
+               testOnReturn=false
+               validationQuery="SELECT 1"
+               jdbcInterceptors="ConnectionState"
+            }
+        }
+    }
 }
-//
-//// environment specific settings
-//environments {
-//    development {
-//        dataSource {
-//            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//        }
-//    }
-//    test {
-//        dataSource {
-//            dbCreate = "update"
-//            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//        }
-//    }
-//    production {
-//        dataSource {
-//            dbCreate = "update"
-//            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//            pooled = true
-//            properties {
-//               maxActive = -1
-//               minEvictableIdleTimeMillis=1800000
-//               timeBetweenEvictionRunsMillis=1800000
-//               numTestsPerEvictionRun=3
-//               testOnBorrow=true
-//               testWhileIdle=true
-//               testOnReturn=true
-//               validationQuery="SELECT 1"
-//            }
-//        }
-//    }
-//}
---
+++ grails-app/domain/me/burson/imemorizedataservice/domain/Fact.groovy
@@ -0,0 +1,21 @@
+package me.burson.imemorizedataservice.domain
+
+@Resource(uri='/fact')
+class Fact {
+
+    String id
+    
+    String question
+    Set<String> possibleAnswers
+    String correctAnswer
+
+    Set<String> references
+
+    Set<String> tags
+
+    // deck, distribution, owner, public, private
+
+    static constraints = {
+        question blank: false, empty: false, nullable:false
+    }
+}
